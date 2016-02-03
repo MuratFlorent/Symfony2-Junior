476954110ecffeec01053deeeb3669ffb9301581
Florent Murat <florentmurat@ymail.com>
2016-02-03 11:45:13 +0100

    Create Tag.php

---
+++ src/s2/todoBundle/Entity/Tag.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace s2\todoBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Tag
+ *
+ * @ORM\Table(name="todo_tags")
+ * @ORM\Entity(repositoryClass="s2\todoBundle\Entity\TagRepository")
+ */
+class Tag {
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=500)
+     */
+    private $name;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId() {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Tag
+     */
+    public function setName($name) {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName() {
+        return $this->name;
+    }
+
+}
